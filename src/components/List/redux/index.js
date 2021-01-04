5bddd5ff0ca134ce6f6800c20e74e9c997fd88c3
Harsha <harzkr142@gmail.com>
2021-01-04 22:54:50 +0530

    Correction to permission pods

--- src/components/List/redux/index.js
+++ src/components/List/redux/index.js
@@ -16,69 +16,69 @@ const get_all_contacts = () => {
 
         const get_contact_permissions_android = () => {
 
-          check(PERMISSIONS.ANDROID.READ_CONTACTS)
-    .then((result) => {
-      switch (result) {
-        case RESULTS.UNAVAILABLE:
-          console.log(
-            'This feature is not available (on this device / in this context)',
-          );
-          break;
-        case RESULTS.DENIED:
-          console.log(
-            'The permission has not been requested / is denied but requestable',
-          );
-      request(
-                    PERMISSIONS.ANDROID.READ_CONTACTS
-                ).then(req_result=>{
-                    if(req_result === 'granted'){
-                        Contacts.getAll().then(contacts => {
-            
-                            //Sorting with full name, is what will be used for display order and search
-                            let _contacts = contacts.map(obj=> ({ ...obj, fullName: obj.givenName + " " + obj.familyName })); 
-                
-                            dispatch(all_contacts(matchSorter(_contacts, '', {keys: ['fullName']})));  
-                          })                           
-                    }
-                    else{
-                        alert("The app requires your permission to read contacts")
-                    }
-                });
-          break;
-        case RESULTS.GRANTED:
-          console.log('The permission is granted');
-          break;
-        case RESULTS.BLOCKED:
-          console.log('The permission is denied and not requestable anymore');
-          break;
-      }
-    })
-    .catch((error) => {
-      console.log("Permissions error",error)
-    });              
-    }
-
-    if(Platform.OS === 'ios'){
-        Contacts.getAll().then(contacts => {
-            // contacts returned
-
-            //Contacts are being mapped through to add another full name field
-            let _contacts = contacts.map(obj=> ({ ...obj, fullName: obj.givenName + " " + obj.familyName })); 
-
-            //Sorting based on the name of the full field
-            dispath(all_contacts(matchSorter(_contacts, '', {keys: ['fullName']}))); 
-          })   
-      }
-      else{
-        //Permissions from Android to read contacts
-        try{
-            get_contact_permissions_android();
+        check(PERMISSIONS.ANDROID.READ_CONTACTS)
+        .then((result) => {
+          switch (result) {
+            case RESULTS.UNAVAILABLE:
+              console.log(
+                'This feature is not available (on this device / in this context)',
+              );
+              break;
+            case RESULTS.DENIED:
+              console.log(
+                'The permission has not been requested / is denied but requestable',
+              );
+            request(
+                PERMISSIONS.ANDROID.READ_CONTACTS
+              ).then(req_result=>{
+                  if(req_result === 'granted'){
+                      Contacts.getAll().then(contacts => {
+          
+                          //Sorting with full name, is what will be used for display order and search
+                          let _contacts = contacts.map(obj=> ({ ...obj, fullName: obj.givenName + " " + obj.familyName })); 
+              
+                          dispatch(all_contacts(matchSorter(_contacts, '', {keys: ['fullName']})));  
+                        })                           
+                  }
+                  else{
+                      alert("The app requires your permission to read contacts")
+                  }
+              });
+              break;
+            case RESULTS.GRANTED:
+              console.log('The permission is granted');
+              break;
+            case RESULTS.BLOCKED:
+              console.log('The permission is denied and not requestable anymore');
+              break;
+            }
+          })
+          .catch((error) => {
+            console.log("Permissions error",error)
+          });              
         }
-        catch(e){
-            console.log(e)
-        }
+
+        if(Platform.OS === 'ios'){
+            Contacts.getAll().then(contacts => {
+                // contacts returned
+
+                //Contacts are being mapped through to add another full name field
+                let _contacts = contacts.map(obj=> ({ ...obj, fullName: obj.givenName + " " + obj.familyName })); 
+
+                //Sorting based on the name of the full field
+                dispatch(all_contacts(matchSorter(_contacts, '', {keys: ['fullName']}))); 
+              })   
+          }
+          else{
+            //Permissions from Android to read contacts
+            try{
+                get_contact_permissions_android();
+            }
+            catch(e){
+                console.log(e)
+            }
+          }
       }
-	}
 
 };
 
